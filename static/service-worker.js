872eb31ed42752969921b274ef6b259f638d7397
YATHARTH <shivaguptaunnao@gmail.com>
2026-08-19 18:08:26 +0530

    Add files via upload

---
+++ static/service-worker.js
@@ -0,0 +1,1 @@
+const CACHE='taskflow-pro-v2';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
